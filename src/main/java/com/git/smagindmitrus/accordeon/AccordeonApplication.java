ced0d644121553f642235a64c551dbb34efadeb6
SmaginDmitrus <smagindm2002@gmail.com>
2025-12-14 12:42:52 +0300

    initialization of app

---
+++ src/main/java/com/git/smagindmitrus/accordeon/AccordeonApplication.java
@@ -0,0 +1,13 @@
+package com.git.smagindmitrus.accordeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccordeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccordeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/smagindmitrus/accordeon/AccordeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.smagindmitrus.accordeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccordeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
